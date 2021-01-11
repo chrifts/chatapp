315caee47c12f189207b302470dc411f2a6a5809
Cristian <schweizercristian@gmail.com>
2021-01-11 20:10:12 +0100

    cors for url

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,21 +5,21 @@ import connectDB from './db';
 const fs = require('fs');
 connectDB();
 
-// const allowedOrigins = ['*', 'http://localhost:8080']; //MAIN APP CORS
+const allowedOrigins = ['https://www.chatapp.website']; //MAIN APP CORS
 // Create a new express app instance
 const app: express.Application = express();
-// app.use(cors({
-//     origin: function(origin, callback){
-//         if(!origin) return callback(null, true);
-//         if(allowedOrigins.indexOf(origin) === -1) {
-//             var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-//             return callback(new Error(msg), false);
-//         }
-//         return callback(null, true);
-//     }
-//     })
-// )
-app.use(cors())
+app.use(cors({
+    origin: function(origin, callback){
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+    })
+)
+// app.use(cors())
 app.use(express.urlencoded({limit: "16mb", extended: true, parameterLimit:50000}));
 app.use(express.json({limit: "16mb"}));
 
